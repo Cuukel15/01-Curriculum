ceded286c6bd247dda9633737c03e8f6f3544b00
Cuukel15 <sow.pape-bilaly@ugb.edu.sn>
2023-04-22 13:16:44 +0000

    first commit

---
+++ golang/printrune.go
@@ -0,0 +1,22 @@
+package golang
+
+import (
+	"errors"
+	"os"
+	"unicode/utf8"
+)
+
+// PrintRune prints a single rune (Unicode code point) and returns any error if the encoding or the writing fails.
+func PrintRune(r rune) error {
+	l := utf8.RuneLen(r)
+	if l == -1 {
+		return errors.New("ERROR: invalid value to encode in UTF-8")
+	}
+
+	p := make([]byte, l)
+
+	utf8.EncodeRune(p, r)
+	_, err := os.Stdout.Write(p)
+
+	return err
+}
